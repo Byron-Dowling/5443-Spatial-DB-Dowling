6bb29fd6b42cba8e8fbbed5ddaa45d99e4554d9a
Byron Dowling <byrondowling7@gmail.com>
2022-12-06 12:52:11 -0600

    Update quit.py

--- Assignments/Missile Defense/quit.py
+++ Assignments/Missile Defense/quit.py
@@ -1,3 +1,13 @@
+"""
+    Name: Byron Dowling
+    Class: 5443 Graduate Spatial Databases
+    
+    File Description:
+      This file stops the missile defense game and calls the missile
+      server with our ID removing us from the list of targets and 
+      returning the region for another team to use.
+"""
+
 import json
 import requests
 
